5772fd0a47a8dc12a56c6b405c534d96fdb22663
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-05-08 13:23:39 +0200

    13:23

--- Anagramma_tomi_uj/Program.cs
+++ Anagramma_tomi_uj/Program.cs
@@ -10,6 +10,30 @@ namespace Anagramma_tomi_uj
     {
         static void Main(string[] args)
         {
+            //1.feladat
+            Console.Write("Kérem adjon meg egy szöveget: ");
+            string szoveg = Console.ReadLine();
+            char[] rendez = szoveg.ToCharArray();
+            Array.Sort(rendez);
+            Console.WriteLine(rendez);
+            int szamol = 1;
+            for (int i = 1;i<rendez.Length;i++)
+            {
+                if (rendez[i-1] == rendez[i])
+                {
+                    szamol++;
+                }
+                else
+                {                  
+                    Console.WriteLine("{0}: {1}", rendez[i - 1], szamol);
+                    szamol = 1;
+                    if (i == rendez.Length -1)
+                    {
+                        Console.WriteLine("{0}: {1}", rendez[i], szamol);
+                    }
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
