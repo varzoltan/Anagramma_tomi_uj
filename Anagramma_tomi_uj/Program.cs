3146e4743586dcf4cedb6b76c6646090f0f38654
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-05-08 14:05:46 +0200

    14.05

--- Anagramma_tomi_uj/Program.cs
+++ Anagramma_tomi_uj/Program.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Anagramma_tomi_uj
 {
+    /*public class szotar
+    {
+        public string szo { get; private set; }
+
+        public szotar(string sor)//konstruktor
+        {
+            szo = sor;
+        }
+    }*/
     class Program
     {
+        struct Adat
+        {
+            public string szo;
+        }
+
         static void Main(string[] args)
         {
             //1.feladat
@@ -33,7 +48,41 @@ namespace Anagramma_tomi_uj
                     }
                 }
             }
+
+            //2.feladat
+            Adat[] adatok = new Adat[300];
+            StreamReader olvas = new StreamReader(@"C:\Users\Rendszergazda\Desktop\2010-okt-Anagramma\szotar.txt");
+            int n = 0;
+            while (!olvas.EndOfStream)
+            {
+                adatok[n].szo = olvas.ReadLine();
+                n++;
+            }
+            olvas.Close();
+            /*List<szotar> adatok = new List<szotar>();//példányosítom a listát
+            //Beolvasás
+            foreach (string i in File.ReadAllLines(@"C:\Users\Rendszergazda\Desktop\2010-okt-Anagramma\szotar.txt"))
+            {
+                adatok.Add(new szotar(i));
+            }*/
+
+            //3.feladat
+            string szo = "valami";
+            Console.WriteLine(abc(szo));
             Console.ReadKey();
+        }
+
+        //függvény
+        static string abc(string szo)
+        {
+            char[] szoszo = szo.ToCharArray();
+            Array.Sort(szoszo);
+            string rendezett = null;
+            for (int i = 0;i<szoszo.Length;i++)
+            {
+                rendezett = rendezett + szoszo[i];
+            }
+            return rendezett;
         }
     }
 }
